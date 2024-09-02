815511d5cd4062a68d2909c14abca8997632bc0c
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-09-02 12:11:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.use(bodyParser.json());
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/students', studentRoutes);
-app.use('/api/teachers', teacherRoutes);
+app.use('/auth', authRoutes);
+app.use('/students', studentRoutes);
+app.use('/teachers', teacherRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
