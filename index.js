bcccae03e41478452401c07dcbc516b604ae9e5b
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-09-02 12:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 
 const authRoutes = require('./routes/auth');
 const studentRoutes = require('./routes/students');
 const teacherRoutes = require('./routes/teachers');
-const bodyParser = require("body-parser")
+
 const app = express();
 
 const DB_URL = process.env.MONGO_URL;
 
-mongoose.connect(DB_URL, {
-})
+mongoose.connect(DB_URL, {})
   .then(() => console.log("Database Connected"))
   .catch(() => console.log("Database not connected"));
 
-
+// Configure CORS to allow requests from any origin
 app.use(cors({
-  origin: ["https://lenvo1.netlify.app/"],
-  methods: ["GET", "POST"],
-  credentials:true
+  origin: '*', // Allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow methods as needed
+  credentials: true
 }));
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
